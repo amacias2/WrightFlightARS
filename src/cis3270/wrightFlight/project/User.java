26260a700cfbd4f49b0895466e6013cabf1a343d
Angela <amchaves27@gmail.com>
2018-04-01 19:29:12 -0400

    User class commit1

---
+++ src/cis3270/wrightFlight/project/User.java
@@ -0,0 +1,125 @@
+package cis3270.wrightFlight.project;
+
+public class User {
+
+
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private String street;
+	private String state;
+	private String country;
+	private String email;
+	private String securityQ;
+	private String securityA;
+	private String ssn;
+	
+	//constructor to fill info for user
+	public User(String firstName, String lastName, String username, String password, String street, String state,
+			String country, String email, String securityQ, String securityA, String ssn) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.street = street;
+		this.state = state;
+		this.country = country;
+		this.email = email;
+		this.securityQ = securityQ;
+		this.securityA = securityA;
+		this.ssn = ssn;
+	}
+	
+	// getters and setters for all attributes
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSecurityQ() {
+		return securityQ;
+	}
+	public void setSecurityQ(String securityQ) {
+		this.securityQ = securityQ;
+	}
+	public String getSecurityA() {
+		return securityA;
+	}
+	public void setSecurityA(String securityA) {
+		this.securityA = securityA;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+	
+	//returns true if username and password passed is same as username and password of user
+	public boolean login(String username, String password) {
+		if(this.username.equals(username)&& this.password.equals(password)) {
+			return true;
+		}
+			return false;
+			
+		}
+	
+	//displays password if string passed equals security answer
+	public void resetLogin(String secuirtyA) {
+		
+		if(this.securityA.equals(secuirtyA)) 
+			System.out.println(this.getPassword());
+	}
+	
+	// override to string to display customer info 
+	@Override
+	public String toString() {
+		return ("Name:"+ this.getFirstName() +" "+ this.getLastName() + "\n Username:" + this.getUsername());
+	}
+	
+	
+}
