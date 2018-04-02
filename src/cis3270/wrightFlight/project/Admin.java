778293ae17aee9ea1e851813503c49b8544f3ded
Angela <amchaves27@gmail.com>
2018-04-01 21:55:34 -0400

    Admin commit1

--- src/cis3270/wrightFlight/project/Admin.java
+++ src/cis3270/wrightFlight/project/Admin.java
@@ -1,5 +1,12 @@
 package cis3270.wrightFlight.project;
 
-public class Admin {
+public class Admin extends User {
 
+	public Admin(String firstName, String lastName, String username, String password, String street, String state,
+			String country, String email, String securityQ, String securityA, String ssn) {
+		super(firstName, lastName, username, password, street, state, country, email, securityQ, securityA, ssn);
+		
+	}
+
+	
 }
